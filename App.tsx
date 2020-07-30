8f3c11978774a824ace34f9e51d8638cb7ec05b6
Nick Rotondo <nickrotondo724@gmail.com>
2020-07-30 11:01:13 -0400

    Add Login skeleton
    Use hooks to fill in name/pw
    Does not send network request
    Rm more template jsx

--- App.tsx
+++ App.tsx
@@ -1,89 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  TextInput,
+  Button,
 } from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 declare const global: { HermesInternal: null | {} };
+
+interface Login {
+  userId: string;
+  token: string;
+}
+const User = React.createContext<Login>({ userId: "", token: "" });
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <View style={styles.body}>
+          <LoginForm />
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
+function LoginForm(): JSX.Element {
+  const [username, updateUsername] = useState("");
+  const [password, updatePassword] = useState("");
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Login</Text>
+      <TextInput
+        style={styles.sectionDescription}
+        placeholder="Username"
+        value={username}
+        onChangeText={updateUsername}
+      />
+      <TextInput
+        style={styles.sectionDescription}
+        placeholder="Password"
+        value={password}
+        onChangeText={updatePassword}
+      />
+      <Button title="Submit" onPress={() => {}} />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: "absolute",
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
@@ -101,17 +74,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "400",
     color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right",
   },
 });
 
