b5c3dbeffa6826a06cd37fd30b2ec53efe6d6243
Nick Rotondo <nickrotondo724@gmail.com>
2020-07-30 21:06:18 -0400

    Add LoginForm, ActiveUser stored in ReactContext
    Dummy component holding the logged in user.

--- App.tsx
+++ App.tsx
@@ -1,87 +1,31 @@
 import React, { useState } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  TextInput,
-  Button,
-  Alert,
-} from "react-native";
+import { SafeAreaView, View, StatusBar, Text } from "react-native";
+import LoginForm from "./src/LoginForm";
+import * as ActiveUser from "./types/active_user";
 
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import login from "./api/login";
-
-interface Login {
-  userId: string;
-  token: string;
-}
-const User = React.createContext<Login>({ userId: "", token: "" });
+const UserContext = React.createContext<ActiveUser.ActiveUser>(
+  ActiveUser.None(),
+);
 
 const App = () => {
+  const [activeUser, updateUser] = useState<ActiveUser.ActiveUser>(
+    ActiveUser.None(),
+  );
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View style={styles.body}>
-          <LoginForm />
-        </View>
+        {activeUser.caseOf({
+          None: () => <LoginForm onLogin={updateUser} />,
+          User: (id, _) => (
+            <View style={{ flex: 1, backgroundColor: "green" }}>
+              <Text>Logged in as: {id}</Text>
+            </View>
+          ),
+        })}
       </SafeAreaView>
     </>
   );
 };
-
-function LoginForm(): JSX.Element {
-  const [username, updateUsername] = useState("");
-  const [password, updatePassword] = useState("");
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Login</Text>
-      <TextInput
-        style={styles.sectionDescription}
-        placeholder="Username"
-        value={username}
-        onChangeText={updateUsername}
-      />
-      <TextInput
-        style={styles.sectionDescription}
-        placeholder="Password"
-        value={password}
-        onChangeText={updatePassword}
-      />
-      <Button
-        title="Submit"
-        onPress={() =>
-          login(username, password)
-            .then(res => Alert.alert("SUCCESS: " + res.data.user.id))
-            .catch(err => Alert.alert("ERR: " + err))
-        }
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark,
-  },
-});
 
 export default App;
---
+++ src/LoginForm.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
+import requestLogin from "../api/login";
+import * as ActiveUser from "../types/active_user";
+
+interface LoginFormProps {
+  onLogin: (user: ActiveUser.ActiveUser) => void;
+}
+
+function LoginForm(props: LoginFormProps): JSX.Element {
+  const [username, updateUsername] = useState("");
+  const [password, updatePassword] = useState("");
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Login</Text>
+      <TextInput
+        style={styles.sectionDescription}
+        placeholder="Username"
+        value={username}
+        onChangeText={updateUsername}
+      />
+      <TextInput
+        style={styles.sectionDescription}
+        placeholder="Password"
+        value={password}
+        onChangeText={updatePassword}
+      />
+      <Button
+        title="Submit"
+        onPress={() =>
+          requestLogin(username, password)
+            .then(res => res.data.user)
+            .then(user => ActiveUser.User(user.id, user.token))
+            .then(props.onLogin)
+            .catch(err => Alert.alert("Login Failed" + err))
+        }
+      />
+    </View>
+  );
+}
+
+// -- STYLES
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: "#FFF",
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400",
+  },
+});
+
+export default LoginForm;
