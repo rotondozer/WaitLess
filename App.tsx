cfc8b6ba05dddf453e6305b96dc20e0098fd203a
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-01 14:19:17 -0400

    Add react navigation
    Build skeleton tabs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
+import "react-native-gesture-handler";
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import LoginForm from "./src/LoginForm";
 import * as ActiveUser from "./types/active_user";
 
@@ -7,23 +10,59 @@ const UserContext = React.createContext<ActiveUser.ActiveUser>(
   ActiveUser.None(),
 );
 
+const Tab = createMaterialTopTabNavigator();
+
 function App(): JSX.Element {
   const [activeUser, updateUser] = useState<ActiveUser.ActiveUser>(
     ActiveUser.None(),
   );
   return (
-    <View style={styles.container}>
-      {activeUser.caseOf({
-        None: () => <LoginForm onLogin={updateUser} />,
-        User: (_, __, email) => (
-          <View style={{ flex: 1, backgroundColor: "green" }}>
-            <Text>Logged in as: {email}</Text>
-          </View>
-        ),
-      })}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        {activeUser.caseOf({
+          None: () => <LoginForm onLogin={updateUser} />,
+          User: (_, __, email) => (
+            <UserContext.Provider value={activeUser}>
+              <View style={{ height: 50 }}>
+                <Text>Hello, {email}!</Text>
+              </View>
+              <Tab.Navigator>
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="Tables" component={Tables} />
+                <Tab.Screen name="Waitlist" component={Waitlist} />
+                <Tab.Screen name="Settings" component={Settings} />
+              </Tab.Navigator>
+            </UserContext.Provider>
+          ),
+        })}
+      </View>
+    </NavigationContainer>
   );
 }
+
+// Placeholder componentws
+
+const Home = () => <View style={{ flex: 1, backgroundColor: "green" }} />;
+
+const Tables = () => <View style={{ flex: 1, backgroundColor: "blue" }} />;
+
+const Waitlist = () => <View style={{ flex: 1, backgroundColor: "pink" }} />;
+
+const Settings = () => (
+  <UserContext.Consumer>
+    {activeUser =>
+      activeUser.caseOf({
+        None: () => null,
+        User: (id, token, email) => (
+          <View style={{ flex: 1, backgroundColor: "yellow" }}>
+            <Text>Logged in as: {email}</Text>
+            <Text>With id: {id}</Text>
+          </View>
+        ),
+      })
+    }
+  </UserContext.Consumer>
+);
 
 // -- STYLES
 
