fee3776d8d6d6b28a271651f88ba162c17c1b862
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-03 09:24:31 -0400

    Add type aliases to ActiveUser strings to self-doc

--- types/active_user.ts
+++ types/active_user.ts
@@ -1,15 +1,20 @@
 import SumType from "sums-up";
 
+// The type aliases help with intellisense to differentiate the args
+type Id = string;
+type Email = string;
+type Token = string;
+
 class ActiveUser extends SumType<{
   None: [];
-  User: [string, string, string];
+  User: [Id, Token, Email];
 }> {}
 
 function None(): ActiveUser {
   return new ActiveUser("None");
 }
 
-function User(id: string, token: string, email: string): ActiveUser {
+function User(id: Id, token: Token, email: Email): ActiveUser {
   return new ActiveUser("User", id, token, email);
 }
 
