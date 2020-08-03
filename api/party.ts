5ae4d6aa67fb97f23fcf708949398390a4757a20
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-03 15:47:04 -0400

    Use Maybes to serialize api response, and render

--- api/party.ts
+++ api/party.ts
@@ -1,17 +1,42 @@
 import axios, { AxiosPromise } from "axios";
+import { Maybe, Nothing } from "seidr";
 import baseUrl from "./base_url";
 import * as ActiveUser from "../types/active_user";
 
 export interface Party {
+  id: string;
   name: string;
-  size: string;
-  // TODO...
-}
-interface GetResponse {
-  parties: Array<Party>;
+  size: number;
+  estimatedWait: string; // what type should this be?
+  notes: string;
+  checkedInAt: Maybe<string>; // this one too
 }
 
-function getAll(activeUser: ActiveUser.ActiveUser): AxiosPromise<GetResponse> {
+interface PartySchema {
+  id: string;
+  name: string;
+  size: string;
+  est_wait: string;
+  notes: string;
+  checked_in?: string;
+}
+function serialize(party: PartySchema): Party {
+  const { id, name, size, est_wait, notes, checked_in } = party;
+  console.log("name: ", name);
+  console.log("id: ", id);
+  return {
+    id,
+    name,
+    size: Maybe.fromNullable(parseInt(size)).getOrElse(0),
+    estimatedWait: est_wait,
+    notes,
+    checkedInAt: Maybe.fromNullable(checked_in),
+  };
+}
+
+function getAll(
+  activeUser: ActiveUser.ActiveUser,
+): Promise<Maybe<Array<Party>>> {
   return activeUser.caseOf({
     None: () => Promise.reject("User Required"),
     User: (id, token, _) =>
@@ -22,7 +47,14 @@ function getAll(activeUser: ActiveUser.ActiveUser): AxiosPromise<GetResponse> {
           "content-type": "application/json",
           Authorization: "Token token=" + token,
         },
-      }),
+      })
+        .then(res =>
+          Maybe.fromNullable(res.data.parties).map(ps => ps.map(serialize)),
+        )
+        .catch(err => {
+          console.warn(err);
+          return Nothing();
+        }),
   });
 }
 
--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -1,27 +1,32 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, Text, StyleSheet, ToastAndroid } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Maybe, Nothing } from "seidr";
 
 import UserContext from "../state/user_context";
 import * as Party from "../api/party";
 
+type PartiesState = Maybe<Array<Party.Party>>;
+
 function WaitList(): JSX.Element {
-  const [parties, updateParties] = useState<Array<Party.Party>>([]); // TODO: maybe!!!!!
+  const [parties, updateParties] = useState<PartiesState>(Nothing());
 
   const user = useContext(UserContext);
 
   useEffect(() => {
-    Party.getAll(user)
-      .then(res => updateParties(res.data.parties))
-      .catch(err => console.warn(err));
+    Party.getAll(user).then(updateParties);
   }, [user]);
 
   return (
     <View style={styles.container}>
-      {parties.map(party => (
-        <Text>
-          Name: {party.name}, Size: {party.size}
-        </Text>
-      ))}
+      {parties.caseOf({
+        Nothing: () => [<Text key={0}>No Parties on the Waitlist!</Text>], // React.Children type or something to get rid of `[]`
+        Just: ps =>
+          ps.map(({ id, name, size }) => (
+            <Text key={id}>
+              Name: {name}, Size: {size}
+            </Text>
+          )),
+      })}
     </View>
   );
 }
