949819eaee6d8be6e3047e177346dbb7d3c57d14
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-05 14:14:38 -0400

    Waitlist imports AddPartyForm

--- src/Waitlist.tsx
+++ src/Waitlist.tsx
@@ -1,12 +1,5 @@
 import React, { ReactNode, useEffect, useState, useContext } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Pressable,
-  ScrollView,
-} from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView } from "react-native";
 import { enableScreens } from "react-native-screens";
 import { createNativeStackNavigator } from "react-native-screens/native-stack";
 import { useNavigation } from "@react-navigation/native";
@@ -14,6 +7,8 @@ import { Maybe, Nothing } from "seidr";
 
 import UserContext from "../state/user_context";
 import * as Party from "../api/party";
+import { Button } from "./common";
+import AddPartyForm from "./AddPartyForm";
 
 enableScreens();
 const Stack = createNativeStackNavigator();
@@ -53,74 +48,14 @@ function WaitList(): JSX.Element {
 
 // -- PRIVATE
 
-// Instad of 'time checked in' input, make it a new Date() to get the current time
-function AddPartyForm(): JSX.Element {
-  const [name, updateName] = useState("");
-  const [partySize, updatePartySize] = useState(""); // numOfGuests instead?
-  const [estWait, updateEstWait] = useState("");
-  const [notes, updateNotes] = useState("");
-
-  return (
-    <View style={[styles.container, { backgroundColor: "purple" }]}>
-      <Text style={styles.title}>Get the party's info...</Text>
-      <View style={{ flexDirection: "row" }}>
-        <TextInput
-          placeholder="Name"
-          value={name}
-          onChangeText={updateName}
-          style={[styles.textInput, { width: "75%" }]}
-        />
-        <TextInput
-          placeholder="# Guests"
-          keyboardType="number-pad"
-          value={partySize}
-          onChangeText={updatePartySize}
-          style={[styles.textInput, { width: "25%", marginLeft: 1 }]}
-        />
-      </View>
-
-      <TextInput
-        placeholder="How long will their wait be?"
-        value={estWait}
-        onChangeText={updateEstWait}
-        style={styles.textInput}
-      />
-      <TextInput
-        placeholder="Notes"
-        value={notes}
-        onChangeText={updateNotes}
-        style={[styles.textInput, { textAlignVertical: "top" }]}
-        multiline
-        editable
-        maxLength={400}
-        numberOfLines={4}
-      />
-      <Pressable
-        onPress={() => {}}
-        style={({ pressed }) =>
-          pressed
-            ? [styles.addPartyButton, styles.buttonPressed]
-            : styles.addPartyButton
-        }>
-        <Text style={styles.buttonText}>Add to Waitlist</Text>
-      </Pressable>
-    </View>
-  );
-}
-
 function AddPartyButton(): JSX.Element {
   const navigation = useNavigation();
 
   return (
-    <Pressable
+    <Button
+      text="Add Party"
       onPress={() => navigation.navigate("Add Party Form")}
-      style={({ pressed }) =>
-        pressed
-          ? [styles.addPartyButton, styles.buttonPressed]
-          : styles.addPartyButton
-      }>
-      <Text style={styles.buttonText}>Add Party</Text>
-    </Pressable>
+    />
   );
 }
 
@@ -144,30 +79,15 @@ function PartyWaiting(party: Party.Party): JSX.Element {
 // -- STYLES
 
 const styles = StyleSheet.create({
-  // TODO: make padding a shared screen option?
-  container: { flex: 1, padding: 5, backgroundColor: "pink" },
-
+  // TODO: share common styles and/or metrics... make padding a shared screen option?
+  container: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: "pink",
+  },
   text: {
     fontSize: 16,
     fontWeight: "bold",
-  },
-  buttonText: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  addPartyButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 35,
-    width: "50%",
-    marginVertical: 10,
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    borderRadius: 12,
-    borderWidth: 3,
-  },
-  buttonPressed: {
-    backgroundColor: "rgba(120, 120, 120, 0.1)",
   },
 
   partyContainer: {
@@ -191,19 +111,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-  },
-
-  // TODO: share common styles and/or metrics
-  textInput: {
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
-    marginTop: 8,
-    borderRadius: 12,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "600",
   },
 });
 
