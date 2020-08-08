fb250be2c50b76613528bca91b561d9c66461195
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-08 17:12:14 -0400

    Rename to parse_int and make SumType

---
+++ util/parse_int.ts
@@ -0,0 +1,36 @@
+import SumType from "sums-up";
+
+/**
+ * `parseInt` API
+ * Since `NaN` is a number, we cannot fully rely on this return type.
+ * Additionally, we can add more specific states for the input,
+ * to provide more tailored error messages
+ */
+class ParseInt extends SumType<{
+  EmptyString: [];
+  NaN: [];
+  Parsed: [number];
+}> {
+  public static parse(str: string): ParseInt {
+    if (str === "") {
+      return EmptyString();
+    } else {
+      const parsed = parseInt(str);
+      return isNaN(parsed) ? NaN() : Parsed(parsed);
+    }
+  }
+}
+
+function EmptyString(): ParseInt {
+  return new ParseInt("EmptyString");
+}
+
+function NaN(): ParseInt {
+  return new ParseInt("NaN");
+}
+
+function Parsed(num: number): ParseInt {
+  return new ParseInt("Parsed", num);
+}
+
+export { ParseInt, EmptyString, NaN, Parsed };
