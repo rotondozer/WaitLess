f2d4b6a3188ea259d0f66d332f592bae7d97dfb2
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-11 14:01:25 -0400

    Add navigation.ts type file

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,6 @@
 import * as ActiveUser from "./active_user";
 import * as ParseInt from "./parse_int";
 
+export * from "./navigation";
+
 export { ActiveUser, ParseInt };
---
+++ types/navigation.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+  Home: { email: string };
+  Settings: { userId: string; token: string; email: string };
+};
