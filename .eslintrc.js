e66e128788e0b8f73d9d32d37829c6ae56e7723f
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-12 10:39:42 -0400

    Add more ts stuff to eslintrc.js...
    run type check with lint script. skipLibCheck in tsconfig
    to avoid getting errors in node_modules, which is not properly respected
    by 'excludes' prop.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   root: true,
-  extends: "@react-native-community",
+  extends: ["@react-native-community", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
     quotes: ["warn", "double"],
+    "@typescript-eslint/no-use-before-define": "off",
+    "react-native/no-inline-styles": "off",
+    "no-shadow-restricted-names": "off",
+    radix: "off",
   },
 };
