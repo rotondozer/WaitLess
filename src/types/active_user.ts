d256183e9f14bd64ed720e4bf0b15ef94e03e7b6
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-16 09:23:44 -0400

    Add map to activeUser

--- src/types/active_user.ts
+++ src/types/active_user.ts
@@ -1,4 +1,5 @@
 import SumType from "sums-up";
+import { Maybe, Nothing, Just } from "seidr";
 
 // The type aliases help with intellisense to differentiate the args
 type Id = string;
@@ -8,7 +9,20 @@ type Token = string;
 class ActiveUser extends SumType<{
   None: [];
   User: [Id, Token, Email];
-}> {}
+}> {
+  /**
+   * Map the ActiveUser with the provided function. ActiveUser.None -> Nothing().
+   * @param f A mapper function that receives the ActiveUser data as an object.
+   */
+  public map<T>(
+    f: (user: { id: Id; token: Token; email: Email }) => T,
+  ): Maybe<T> {
+    return this.caseOf({
+      None: () => Nothing(),
+      User: (id, token, email) => Just(f({ id, token, email })),
+    });
+  }
+}
 
 function None(): ActiveUser {
   return new ActiveUser("None");
