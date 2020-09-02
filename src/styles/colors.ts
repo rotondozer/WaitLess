67c0db022880284bf000f63eff5592910a2736ec
Nick Rotondo <nickrotondo724@gmail.com>
2020-09-02 09:04:41 -0400

    Add styles/colors and include a palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+const darkBlue = "#313872";
+const orange = "#ea8d2f";
+const tan = "#dfd093";
+const grayWhite = "#e7e7e7";
+const lightBlue = "#2ad1df";
+
+export { darkBlue, orange, tan, grayWhite, lightBlue };
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
+import * as Colors from "./colors";
 import * as Fonts from "./fonts";
 import * as Layouts from "./layouts";
 
-export { Fonts, Layouts };
+export { Colors, Fonts, Layouts };
