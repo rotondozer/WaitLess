a932cd7661fcb730be608ba81132d47b3b17d792
Nick Rotondo <nickrotondo724@gmail.com>
2020-09-03 12:11:52 -0400

    Remove unused, inline exports

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,40 +1,8 @@
-const darkBlue = "#313872";
-const orange = "#ea8d2f";
-const tanOpaque = "#d7d7ce80";
-const tanGreen = "#8e8e76";
-const grayBrown = "#6b6b67";
-const grayWhite = "#e8e8e3";
-const grayWhiteOpaque = "#e7e7e790";
-const lightBlue = "#2ad1df";
-const lightBlueOpaque = "#2ad1df70";
-const teal = "#62a199";
-const purple = "#8E768E";
-
-const blueGray = "#76828e";
-const sageGray = "#768E8E";
-const blue = "#677e97";
-const blackish = "#1c1c17";
-const whitish = "#e1e1e0";
-const darkRed = "#580420";
-const red420 = "#420420";
-
-export {
-  darkBlue,
-  orange,
-  darkRed,
-  tanOpaque,
-  tanGreen,
-  sageGray,
-  grayBrown,
-  grayWhite,
-  grayWhiteOpaque,
-  lightBlue,
-  lightBlueOpaque,
-  teal,
-  purple,
-  blackish,
-  blueGray,
-  blue,
-  whitish,
-  red420,
-};
+export const blackish = "#1c1c17"; // fonts
+export const blue = "#677e97"; // primary color alt
+export const blueGray = "#76828e"; // primary color
+export const darkRed = "#580420"; // border, accent
+export const red420 = "#420420"; // border, accent
+export const sageGray = "#768E8E"; // UNUSED... but could be?
+export const tanOpaque = "#d7d7ce80"; // text field background
+export const whitish = "#e1e1e0"; // background
