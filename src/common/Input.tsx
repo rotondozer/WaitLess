e79dc98307007333187960a87e17617643d3adb6
Nick Rotondo <nickrotondo724@gmail.com>
2020-09-03 13:21:02 -0400

    Modify custom Input to include `withLabel`  prop

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -1,19 +1,43 @@
 import React from "react";
-import { TextInput, TextInputProps, StyleSheet } from "react-native";
+import {
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from "react-native";
+import { Colors } from "styles";
 
-function Input(props: TextInputProps): JSX.Element {
-  return <TextInput {...props} style={[styles.textInput, props.style]} />;
+interface Props extends TextInputProps {
+  withLabel?: string;
+  containerStyle?: ViewStyle;
+}
+
+function Input(props: Props): JSX.Element {
+  return (
+    <View style={[styles.container, props.containerStyle]}>
+      {props.withLabel && <Text style={styles.label}>{props.withLabel}</Text>}
+      <TextInput {...props} style={[styles.textInput, props.style]} />
+    </View>
+  );
 }
 
 // -- STYLES
 
 const styles = StyleSheet.create({
   textInput: {
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
-    marginTop: 8,
-    borderRadius: 12,
+    backgroundColor: Colors.whitish,
+    borderRadius: 5,
     fontSize: 18,
     fontWeight: "400",
+  },
+  label: {
+    marginVertical: 3,
+    letterSpacing: 0.5,
+  },
+  container: {
+    margin: 8,
   },
 });
 
